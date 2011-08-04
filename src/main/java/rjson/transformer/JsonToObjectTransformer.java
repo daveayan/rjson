47d9aa0653f2836a8fb6b1065f50b93db67ea631
Ayan Dave <daveayan@gmail.com>
2011-08-04 19:06:03 -0400

    Removed the Transformer interface. This is no longer needed.

--- src/main/java/rjson/transformer/JsonToObjectTransformer.java
+++ src/main/java/rjson/transformer/JsonToObjectTransformer.java
@@ -25,7 +25,7 @@ package rjson.transformer;
 
 import rjson.Rjson;
 
-public interface JsonToObjectTransformer extends Transformer {
+public interface JsonToObjectTransformer {
 	public Object transformJsonToObject(Object object, Rjson rjson);
 
 	public boolean canConvertToObject(Object object);
