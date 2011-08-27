266374c1e5ef735fc6ca0c2372194f695fb26b90
Ayan Dave <daveayan@gmail.com>
2011-08-27 15:07:19 -0400

    Fixed a incomplete unit test

--- src/test/java/org/json/JSONObject_equals_Test.java
+++ src/test/java/org/json/JSONObject_equals_Test.java
@@ -4,11 +4,12 @@ import org.junit.Test;
 
 import rjson.domain.Person;
 import rjson.utils.RjsonUtil;
+import zen.Assert;
 
 public class JSONObject_equals_Test {
 	@Test public void verifyStringComparisions() throws JSONException {
-		JSONObject jo1 = new JSONObject(RjsonUtil.completeSerializer().toJson(Person.getFullyLoadedInstanceWithNullAddress()));
+		JSONObject jo1 = new JSONObject(RjsonUtil.completeSerializer().toJson(Person.getFullyLoadedInstance()));
 		JSONObject jo2 = new JSONObject(RjsonUtil.completeSerializer().toJson(Person.getFullyLoadedInstance()));
-		boolean equals = jo1.equals(jo2);
+		Assert.thatEquals(jo1, jo2);
 	}
 }
